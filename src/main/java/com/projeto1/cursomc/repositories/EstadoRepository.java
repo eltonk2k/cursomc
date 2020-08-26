459661cfdd3f8bc19a1ab62de65ddf73a11349a5
eltonk2k <elieltonpb1@gmail.com>
2020-08-26 12:13:12 -0300

    Estado e cidade

---
+++ src/main/java/com/projeto1/cursomc/repositories/EstadoRepository.java
@@ -0,0 +1,15 @@
+package com.projeto1.cursomc.repositories;
+
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
+
+import com.projeto1.cursomc.domain.Estado;
+
+@Repository
+public interface EstadoRepository extends JpaRepository<Estado, Integer> {
+	
+	
+	
+	
+
+}
